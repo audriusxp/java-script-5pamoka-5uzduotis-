e9b0490b0bbdc4118361696f17b9a639145ea916
audriusxp <palm@inbox.lt>
2022-06-16 08:35:05 +0300

    parasyta pirma funkcija ,bet rezultatas ne toks kokios nurodytos salygos

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 window.onload = function() {
+    const testinis = [5,12,3,6,8,14,15,5,6,10];
+    console.log(testinis);
+    const didesninei10 = isaugotiDidesnius10();
+    console.log(didesninei10);
     
+    
+}
+function isaugotiDidesnius10(){
+    const testinis = [];
+    for (let i = 0; i < testinis.length; i++) {
+        if (daugiau(i)) ;
+        rezultas.push(i);
+        
+    }
+    return testinis;
+}
+
+function daugiau(x){
+    return (x) > 10;
 }
